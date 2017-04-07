ab9d28adda449ead471d4c42c3689d9e462d91b8
Akbar Gumbira <akbargumbira@gmail.com>
2017-04-07 15:46:45 +0200

    Add CNN testing on MNIST.

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
+import os
 import keras
 from keras.datasets import mnist
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
@@ -10,7 +11,7 @@ import numpy as np
 
 batch_size = 128
 num_classes = 10
-epochs = 1
+epochs = 5
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -44,26 +45,33 @@ print(x_test.shape[0], 'test samples')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = Sequential()
-model.add(
-    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+# # Training
+# model = Sequential()
+# model.add(
+#     Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(num_classes, activation='softmax'))
+#
+# model.compile(loss=keras.losses.categorical_crossentropy,
+#               optimizer=keras.optimizers.Adadelta(),
+#               metrics=['accuracy'])
+#
+# model.fit(x_train, y_train,
+#           batch_size=batch_size,
+#           epochs=epochs,
+#           verbose=1,
+#           validation_data=(x_test, y_test))
+#
+# # Save trained model
+# model.save(os.path.join(os.path.curdir, 'model', 'cnn_mnist_5.h5'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
-
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
+# Load model
+model = load_model('model/cnn_mnist_5.h5')
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
